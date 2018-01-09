5c0ec65ae84ddcc12ccd133e8964ce3c0b0c514b
Limor "Ladyada" Fried <limor@ladyada.net>
2018-01-09 15:55:09 -0500

    use struct when available, change map() to list comp (#4)

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -23,7 +23,7 @@ while True:
     accel_x, accel_y, accel_z = sensor.accelerometer
     mag_x, mag_y, mag_z = sensor.magnetometer
     # Print values.
-    print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
-    print('Magnetometer (uTesla): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
+    print('Acceleration (m/s^2): ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(accel_x, accel_y, accel_z))
+    print('Magnetometer (uTesla): ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(mag_x, mag_y, mag_z))
     # Delay for a second.
     time.sleep(1.0)
